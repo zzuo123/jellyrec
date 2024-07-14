78c4d9a0829eafa13d786455897fa3d82be1150d
Zhiyang Zuo <zzuo@umass.edu>
2024-07-14 18:21:18 -0400

    added more api endpoints

--- backend/modules/scrape/scrape.js
+++ backend/modules/scrape/scrape.js
@@ -1,6 +1,10 @@
+// ============================================================
+// For recommendations based on the most recent shows/movies
+// ============================================================
+
 async function getMostRecentShows(baseurl, uid, token){
+    // get the most recently watched shows
     const url = baseurl+"/api/Users/"+uid+"/Items?Recursive=true&IncludeItemTypes=Episode&SortBy=DatePlayed&SortOrder=Descending&Fields=DatePlayed";
-    // to be changed using new api in POSTMAN
     const response = await fetch(url, {
         method: "GET",
         headers: {
@@ -16,6 +20,67 @@ async function getMostRecentShows(baseurl, uid, token){
     return result;
 }
 
+async function getMostRecentMovies(baseurl, uid, token){
+    // get the most recently watched movies
+    const url = baseurl+"/api/Users/"+uid+"/Items?Recursive=true&IncludeItemTypes=Movie&SortBy=DatePlayed&SortOrder=Descending&Fields=DatePlayed";
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": 'MediaBrowser Client="JellyRec", Device="JellyRecBackend", DeviceId="JellyRecBackend", Version="1.0.0", Token="'+token+'"'
+        }
+    });
+    if (response.status != 200) {
+        return null;
+    }
+    const result = await response.json();
+    console.log(result);
+    return result;
+}
+
+// ============================================================
+// For recommendations based on the favorited shows/movies`
+// ============================================================
+
+async function getFavShows(baseurl, uid, token){
+    const url = baseurl+"/api/Users/"+uid+"/Items?Recursive=true&SortBy=DatePlayed&SortOrder=Descending&Fields=DatePlayed&IncludeItemTypes=Show&isFavorite=true";
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": 'MediaBrowser Client="JellyRec", Device="JellyRecBackend", DeviceId="JellyRecBackend", Version="1.0.0", Token="'+token+'"'
+        }
+    });
+    if (response.status != 200) {
+        return null;
+    }
+    const result = await response.json();
+    console.log(result);
+    return result;
+}
+
+async function getFavMovies(baseurl, uid, token){
+    const url = baseurl+"/api/Users/"+uid+"/Items?Recursive=true&SortBy=DatePlayed&SortOrder=Descending&Fields=DatePlayed&IncludeItemTypes=Movie&isFavorite=true";
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": 'MediaBrowser Client="JellyRec", Device="JellyRecBackend", DeviceId="JellyRecBackend", Version="1.0.0", Token="'+token+'"'
+        }
+    });
+    if (response.status != 200) {
+        return null;
+    }
+    const result = await response.json();
+    console.log(result);
+    return result;
+}
+
+
+// ========================= EXPORT =========================
 export default {
-    getMostRecentShows
+    getMostRecentShows,
+    getMostRecentMovies,
+    getFavShows,
+    getFavMovies
 }
