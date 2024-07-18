b63ad7b8185521f6d0bf2e36acc455151eb90bfd
Zhiyang Zuo <zzuo@umass.edu>
2024-07-18 16:27:54 -0400

    scraper extract fields and image url

--- backend/modules/scraper/scraper.js
+++ backend/modules/scraper/scraper.js
@@ -2,6 +2,26 @@
 // For recommendations based on the favorited shows/movies`
 // ============================================================
 
+// <base-url>/Items/<item-id>/Images/Primary?fillHeight=720&fillWidth=480&quality=96&tag=<primary-image-tag>
+function generateImageUrl(baseurl, itemid, tag) {
+    if (tag == null || tag == undefined) {
+        return undefined;
+    }
+    return baseurl+"/Items/"+itemid+"/Images/Primary?fillHeight=720&fillWidth=480&quality=96&tag="+tag;
+}
+
+// Fields we want: Name, Id, ProviderIds, imageurl
+function extractFields(result, baseurl) {
+    return result["Items"].map(item => {
+        return {
+            id: item.Id,
+            name: item.Name,
+            imdbid: item.ProviderIds.Imdb,
+            imageurl: generateImageUrl(baseurl, item.Id, item.ImageTags.Primary)
+        }
+    });
+}
+
 async function getFavShows(baseurl, uid, token){
     const url = baseurl+"/Users/"+uid+"/Items?Recursive=true&Fields=ProviderIds&IncludeItemTypes=Series&isFavorite=true";
     const response = await fetch(url, {
@@ -15,7 +35,7 @@ async function getFavShows(baseurl, uid, token){
         return null;
     }
     const result = await response.json();
-    return result;
+    return extractFields(result, baseurl);
 }
 
 async function getFavMovies(baseurl, uid, token){
@@ -31,7 +51,7 @@ async function getFavMovies(baseurl, uid, token){
         return null;
     }
     const result = await response.json();
-    return result;
+    return extractFields(result, baseurl);
 }
 
 
