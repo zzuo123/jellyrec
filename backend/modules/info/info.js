3f86590f49d681b3ed384c968ee4087e7c7da919
Zhiyang Zuo <zzuo@umass.edu>
2024-07-31 18:23:55 -0400

    cached omdb data to save api calls

--- backend/modules/info/info.js
+++ backend/modules/info/info.js
@@ -1,23 +1,30 @@
-// https://www.omdbapi.com/?i=tt3896198&apikey={OMDB_API_KEY}
+// https://www.omdbapi.com/?i={IMDB_ID}&apikey={OMDB_API_KEY}
 
-async function get_info(imdb_id, omdb_api_key){
-    const url = `https://www.omdbapi.com/?i=${imdb_id}&apikey=${omdb_api_key}`;
-    console.log(url);
-    const response = await fetch(url);
-    if (!response.ok) {
-        return null;
-    }
-    const result = await response.json();
-    return result;
+import * as fs from 'fs';
+import logger from '../log/logger.js';
+
+// when module is imported, read file or create empty array if file does not exist
+let info = [];
+if (fs.existsSync('omdb_cache.json')) {
+    info = JSON.parse(fs.readFileSync('omdb_cache.json'));
 }
 
+async function get_info(imdb_id, omdb_api_key){
+    let result = info.find((element) => element.imdbID === imdb_id);
+    if (result !== undefined) { return result; }
+    const url = `https://www.omdbapi.com/?i=${imdb_id}&apikey=${omdb_api_key}`;
+    logger.info(`GET ${url}: requesting movie info`);
+    const response = await fetch(url);
+    if (!response.ok) { return result; }
+    result = await response.json();
+    info.push(result);
+    return result;
+}
 
 async function add_info_omdb(movies, omdb_api_key){
     for (let i = 0; i < movies.length; i++) {
         const info = await get_info(movies[i].imdb_id, omdb_api_key);
-        if (info === null) {
-            continue;
-        }
+        if (info === null) { continue; }
         movies[i].title = info.Title || "N/A";
         movies[i].released = info.Released || "N/A";
         movies[i].rating = info.imdbRating || "N/A";
@@ -30,7 +37,7 @@ async function add_info_omdb(movies, omdb_api_key){
         movies[i].runtime = info.Runtime || "N/A";
         movies[i].language = info.Language || "N/A";
     }
-    // console.log(movies);
+    fs.writeFileSync('omdb_cache.json', JSON.stringify(info));
     return movies;
 }
 
