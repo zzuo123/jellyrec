d289f0844b4d32f7018a8a991749f68c59ade7de
Zhiyang Zuo <zzuo@umass.edu>
2024-09-03 15:38:08 -0400

    refactored sveltekit login to check main server

--- frontend/src/routes/login/+page.server.js
+++ frontend/src/routes/login/+page.server.js
@@ -1,6 +1,37 @@
 import { user } from "$lib/stores/user.js";
 import { redirect } from "@sveltejs/kit";
 
+async function login(url, name, pw) {
+  const response = await fetch("http://localhost:4001/Auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: url,
+      username: name,
+      password: pw,
+    }),
+  });
+  console.log("login: request sent");
+  return response;
+}
+
+async function checkLogin() {
+  let result = await fetch("http://localhost:4001/Auth/loggedin", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (result.ok) {
+    result = await result.json();
+    return result.username;
+  } else {
+    return null;
+  }
+}
+
 export const actions = {
   default: async ({ cookies, request, fetch }) => {
     console.log("login: action called");
@@ -25,19 +56,17 @@ export const actions = {
       };
     }
 
-    const response = await fetch("http://localhost:4001/Auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        url: url,
-        username: name,
-        password: pw,
-      }),
-    });
+    if (checkLogin() != null) {
+      console.log("login: user already logged in");
+      fetch("http://localhost:4001/Auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
 
-    console.log("login: request sent");
+    let response = await login(url, name, pw);
 
     if (response.ok) {
       console.log("login: response ok");
@@ -52,11 +81,9 @@ export const actions = {
         name: name,
       };
       user.set(obj);
-
       throw redirect(302, "/home");
     } else {
       console.log("login: response not ok");
-      const { errors } = await response.json();
       return {
         status: 400,
         body: {
